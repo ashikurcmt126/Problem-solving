3d769e443dd5b6af640aa84cdca234ff787c0a12
ashikurcmt126 <56148949+ashikurcmt126@users.noreply.github.com>
2021-09-18 23:26:13 +0600

    Update 1.TwoSum.java
    updated solution of twoSum

--- Leetcode/1.TwoSum.java
+++ Leetcode/1.TwoSum.java
@@ -11,6 +11,21 @@ public class Main
         }  
         return new int[]{};
     }
+	
+    /*
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(mp.containsKey(target - nums[i])){
+                return new int[]{mp.get(target - nums[i]), i};
+            }else{
+                mp.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+    }
+   */
     
 	public static void main(String[] args) {
 	    int a[] = new int[] {2,7,11,15};
